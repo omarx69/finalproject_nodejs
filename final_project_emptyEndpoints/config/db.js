893cf16277a11fe3e877789823b92406faba22df
Omar Mohamed <121226656+omarx69@users.noreply.github.com>
2025-07-03 18:33:10 +0300

    Update db.js

--- final_project_emptyEndpoints/config/db.js
+++ final_project_emptyEndpoints/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = () => {
-    mongoose.connect('mongodb+srv://hssnomr2022:VAyvcBetc4YJtsBp@cluster0.qlj1kug.mongodb.net/wishlistDB?retryWrites=true&w=majority&appName=Cluster0')
+    mongoose.connect('mongodb+srv://<username>:<password>@cluster0.qlj1kug.mongodb.net/wishlistDB?retryWrites=true&w=majority&appName=Cluster0')
     .then(() => {
         console.log("Connection is successful");
     })
