08dc8746210ff792447b6eb85ac839af48edbc7e
Omar <gitomar69@gmail.com>
2025-07-10 20:51:12 +0300

    Adding the admin email

--- final_project_emptyEndpoints/middlewares/isAdmin.js
+++ final_project_emptyEndpoints/middlewares/isAdmin.js
@@ -1,3 +1,5 @@
+const ADMIN_EMAIL = "admin@gmail.com";
+
 const isAdmin = (req, res, next) => {
     if (!req.user || req.user.email !== ADMIN_EMAIL) {
         return res.status(403).json({ message: 'Admin access required' });
